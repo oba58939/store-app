387ab8bdd3cd9c4d049f0da0eaf665043417aaf4
oba58939 <ohbahinako@gmail.com>
2025-01-21 17:10:34 +0900

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet, Routes } from '@angular/router';
+import { ResultComponent } from './result/result.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'store-app';
+  title = '在庫管理システム';
+  keyword = ''
+  
+selectedStore: string = '';
+selectedJanru: string = '';
+
 }
+
+export const routes: Routes = [{ path: 'result-component', component: ResultComponent },];
+
